2dda0b4fd82e02215634dc021a89ce5b8e3415af
det92drole <det92drole@gmail.com>
2026-05-09 18:13:54 -0500

    PWA update

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//REGISTER serviceWorker
+
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker.register("./sw.js")
+            .then(() => {
+                console.log("SW registered");
+            })
+            .catch((err) => {
+                console.log("SW failed", err);
+            });
+
+    });
+
+}
+
 // DECLARED VARIABLES:
 
 const modal = document.getElementById("modal"); //modal menu
